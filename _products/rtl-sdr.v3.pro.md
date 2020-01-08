ca12b5b1b060cde6f0c9c99ad2e7c6e4e55d55f5
w221 <59580325+w221@users.noreply.github.com>
2020-01-08 03:08:50 +0200

    fre
    fre

---
+++ _products/rtl-sdr.v3.pro.md
@@ -0,0 +1,21 @@
+---
+title: RTL-SDR V3 PRO
+description: SDR-приемник
+price: '750.00'
+<!-- sizes:
+  - Small
+  - Medium
+  - Large
+  - XL -->
+styles:
+  - name: Gold
+    color: '#000000'
+    image_path: /images/products/rtl-sdr.v3.pro/RTL-SDR-RTL2832-SDR-R820T2-100-1.jpg
+
+featured_image_path: /images/products/rtl-sdr.v3.pro/RTL-SDR-RTL2832-SDR-R820T2-100-1.jpg
+facebook_image_path:
+---
+
+Sacha’s elegant antlers have never been seen on such a beautiful t-shirt. Wear this majestic beast and feel all your problems float away.
+
+Slim Fit, 5oz 100% Cotton T-Shirt.
--- js/simpleCart.js
+++ js/simpleCart.js
@@ -82,13 +82,14 @@
 					"SEK": { code: "SEK", symbol: "SEK&nbsp;", name: "Swedish Krona" },
 					"CHF": { code: "CHF", symbol: "CHF&nbsp;", name: "Swiss Franc" },
 					"THB": { code: "THB", symbol: "&#3647;", name: "Thai Baht" },
-					"BTC": { code: "BTC", symbol: " BTC", name: "Bitcoin", accuracy: 4, after: true	}
+					"BTC": { code: "BTC", symbol: " BTC", name: "Bitcoin", accuracy: 4, after: true	},
+					"UAH": { code: "UAH", symbol: " грн.", name: "Гривня",  after: true	}
 				},
 
 				// default options
 				settings = {
 					checkout				: { type: "PayPal", email: "you@yours.com" },
-					currency				: "USD",
+					currency				: "UAH",
 					language				: "english-us",
 
 					cartStyle				: "div",
